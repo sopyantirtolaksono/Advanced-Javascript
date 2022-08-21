85e10b4c706d1d1ee7a81a921b8c15bfc16a264c
sopyantirtolaksono <laksonotirtosopyant@gmail.com>
2022-08-22 06:50:53 +0700

    create object with function declaration

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 // CREATE OBJECT IN JAVASCRIPT
 
 // 1. Object Literal
-let student1 = {
-	name: "Sopyan",
-	energy: 90,
-	eat: function(portion) {
-		this.energy = this.energy + portion;
-		console.log(`Hello ${this.name}, your energy is ${this.energy}. Happy eat:)`);
-	}
-};
+// let student1 = {
+// 	name: "Sopyan",
+// 	energy: 90,
+// 	eat: function(portion) {
+// 		this.energy = this.energy + portion;
+// 		console.log(`Hello ${this.name}, your energy is ${this.energy}. Happy eat:)`);
+// 	}
+// };
 
-let student2 = {
-	name: "Ferry",
-	energy: 50,
-	eat: function(portion) {
-		this.energy = this.energy + portion;
+// let student2 = {
+// 	name: "Ferry",
+// 	energy: 50,
+// 	eat: function(portion) {
+// 		this.energy = this.energy + portion;
+// 		console.log(`Hello ${this.name}, your energy is ${this.energy}. Happy eat:)`);
+// 	}
+// };
+
+// 2. Function Declaration
+function Student(name, energy) {
+	let student = {};
+
+	student.name 	= name;
+	student.energy 	= energy;
+	student.eat = function(portion) {
+		this.energy += portion;
 		console.log(`Hello ${this.name}, your energy is ${this.energy}. Happy eat:)`);
 	}
-};
+	student.play = function(hours) {
+		student.energy -= hours;
+		console.log(`Hello ${this.name}, your energy is ${this.energy}. Happy play:)`);
+	}
+
+	return student;
+}
+
+let sopyan 	= Student("Sopyan", 90);
+let ferry	= Student("Ferry", 50);
