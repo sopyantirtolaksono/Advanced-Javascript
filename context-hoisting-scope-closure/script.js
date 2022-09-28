ef09eef523d7cae5de3b391cd242650e820c9eb1
sopyantirtolaksono <laksonotirtosopyant@gmail.com>
2022-09-29 02:41:45 +0700

    update & add comment on context, hoisting, scope

--- context-hoisting-scope-closure/script.js
+++ context-hoisting-scope-closure/script.js
@@ -1,21 +1,29 @@
 // CONTEXT-HOISTING-SCOPE-CLOSURE
 
-// 1. Execution Context, Hoisting & Scope
+// 1. Execution Context(Creation phase - Execution phase), Hoisting & Scope
+// Creation phase & Hoisting
 function one() {
 	var nama = "Sopyan";
 	console.log(nama);
 }
 
+// Creation phase & Hoisting
 function two() {
+	// Scope(Access to global scope/window)
 	console.log(nama);
 }
 
+// Execution phase
 console.log(nama);
 
+// Creation phase & Hoisting
 var nama = "Ferry";
 
+// Execution phase
 one();
 
+// Execution phase
 two("Indah");
 
+// Execution phase
 console.log(nama);
