50afded0fda52a9297d24b9ef26c44e5ce1cc281
sopyantirtolaksono <laksonotirtosopyant@gmail.com>
2022-11-29 03:20:20 +0700

    update keyword this in arrow function

--- arrow-function/script.js
+++ arrow-function/script.js
@@ -1,7 +1,7 @@
 // ARROW-FUNCTION
 
 // Create array students
-let students = ["Sopyan", "Yohanes", "Chabib"];
+// let students = ["Sopyan", "Yohanes", "Chabib"];
 
 // Map students with one line
 // let student = students.map(name => name.length);
@@ -35,11 +35,34 @@ let students = ["Sopyan", "Yohanes", "Chabib"];
 // });
 
 // Map students & create new object
-let student = students.map(name => ({
-    "name" : name,
-    "length" : name.length
-}));
+// let student = students.map(name => ({
+//     "name" : name,
+//     "length" : name.length
+// }));
 
 // Print the result
 // console.log(room);
+// console.log(student);
+
+
+
+// Keyword "This" in arrow function
+
+// Implementation on constructor function (with function declaration/expression not arrow function)
+// Keyword "This" can't use in arrow function
+// Arrow function not have a concept "This"
+// Method can use arrow function
+let Students = function() {
+    this.name   = "Sopyan",
+    this.gender = "Male",
+    this.age    = 17,
+
+    this.sayHello = () => {
+        console.log("Hello, " + this.name);
+    }
+}
+
+// init new object(Students)
+let student = new Students();
+// Print result
 console.log(student);
